f0472fbd4d71f4ceabea7d5780e8c72c07618ee0
nextlevelengtech <dangekalpesh9@gmail.com>
2024-11-03 23:02:31 +0530

    Added new learning content.

---
+++ Chapter 1/Characters_and_strings_L2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	// Defining a character data type variable and printing the value assigned to it.
+	char c_var = 'A';
+	printf("Print the data variable 'c_var' of character type holds: %c\n", c_var);
+
+	// Checking the size of character data type
+
+	// Defining a string data type variable and printing the value assigned to it.
+	char s_var[] = "Hello";  // String data type is an array of characters, the array may or may not be defined.
+	printf("Print the data variable 's_var' of string type holds: %s\n", s_var);
+
+	// Checking the size of character data type
+
+	return 0;
+}
