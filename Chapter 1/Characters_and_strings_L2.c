b3d7a0771045bdaf0593ba09ecdb3f0eb1863197
nextlevelengtech <dangekalpesh9@gmail.com>
2024-11-03 23:07:18 +0530

    Added size calculation of char and string data type.

--- Chapter 1/Characters_and_strings_L2.c
+++ Chapter 1/Characters_and_strings_L2.c
@@ -7,12 +7,16 @@ int main()
 	printf("Print the data variable 'c_var' of character type holds: %c\n", c_var);
 
 	// Checking the size of character data type
+	int c_var_size = sizeof(c_var);
+	printf("Size of characater data type variable 'c_var' is: %d\n", c_var_size);
 
 	// Defining a string data type variable and printing the value assigned to it.
 	char s_var[] = "Hello";  // String data type is an array of characters, the array may or may not be defined.
 	printf("Print the data variable 's_var' of string type holds: %s\n", s_var);
 
 	// Checking the size of character data type
+	int s_var_size = sizeof(s_var);
+	printf("Size of string data type variable 's_var' is: %d\n", s_var_size);
 
 	return 0;
 }
