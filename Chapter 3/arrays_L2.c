da99616addfa378a0caa8c178dfc674436969615
nextlevelengtech <dangekalpesh9@gmail.com>
2024-11-05 13:36:30 +0530

    Added the code comments

--- Chapter 3/arrays_L2.c
+++ Chapter 3/arrays_L2.c
@@ -4,12 +4,13 @@
 
 int middleNumber(int arr_ip[], int arr_len)
 {
-	int arr_odd_check = arr_len%2;
-	printf("%d\n", arr_odd_check);
-	if(arr_odd_check !=0)
+	//int arr_odd_check = arr_len%2;
+	//printf("%d\n", arr_odd_check);
+	if(arr_len%2 !=0)  // Checking if the array length is odd so that it has a middle number
 	{
-		int middle_num = *(arr_ip + arr_len/2);
-		printf("%d\n", middle_num);
+		// Add the address of the middle number to the first element address and dereference its value
+		int middle_num = *(arr_ip + arr_len/2);  
+		// printf("%d\n", middle_num);
 		return middle_num;
 	}
 	
@@ -18,6 +19,7 @@ int middleNumber(int arr_ip[], int arr_len)
 int main()
 {
 	int arr[N] =  {1,2,3,4,5};  // Defining a fixed size array
-	int arr_mid = middleNumber(arr ,N);
+	// Passing the pointer to first array element and Number of array elements as arguments to 'middleNumber' function
+	int arr_mid = middleNumber(arr ,N);  
 	printf("The middle number of the array is:%d\n", arr_mid);
 }
