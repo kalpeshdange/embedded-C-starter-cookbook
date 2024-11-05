5dadc94f2b4e99e0cf3a333c54693ded15f61384
nextlevelengtech <dangekalpesh9@gmail.com>
2024-11-05 13:32:57 +0530

    Added middle number cal

--- Chapter 3/arrays_L2.c
+++ Chapter 3/arrays_L2.c
@@ -2,14 +2,14 @@
 
 #define N 5  // Macro for array size definition
 
-int middleNumber(int arr_ip, int arr_len)
+int middleNumber(int arr_ip[], int arr_len)
 {
 	int arr_odd_check = arr_len%2;
 	printf("%d\n", arr_odd_check);
 	if(arr_odd_check !=0)
 	{
-		int middle_num = sizeof(arr_ip)/arr_len;
-		printf("%d\n", sizeof(arr_ip));
+		int middle_num = *(arr_ip + arr_len/2);
+		printf("%d\n", middle_num);
 		return middle_num;
 	}
 	
@@ -18,6 +18,6 @@ int middleNumber(int arr_ip, int arr_len)
 int main()
 {
 	int arr[N] =  {1,2,3,4,5};  // Defining a fixed size array
-	int arr_mid = middleNumber(arr[N] ,N);
+	int arr_mid = middleNumber(arr ,N);
 	printf("The middle number of the array is:%d\n", arr_mid);
 }
